ddd0215f48e02d49a05bb67bb8a39c812d6e019c
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-07 16:05:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   get "say/:number/:phrase" do
     @number2 = params[:number]
     @phrase = params[:phrase]
-    @number2.to_i.times {"#{@phrase}"}
+    @number2.to_i.times {puts "#{@phrase}"}
 
 
   end
