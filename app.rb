9cff80cc24848504056441fa857563151ddeb7cc
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-07 15:46:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  
+  get "/reversename/:name" do
+    @username=params[:name]
+    reversed_name=@username.reverse
+    "Is your name #{reversed_name}? or #{@username}"
+  end
 
 
 end
